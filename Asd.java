ee3a031d7217de7157d427addcc523ba12d37f76
pulykaman <35999258+pulykaman@users.noreply.github.com>
2018-04-12 12:24:46 +0200

    Create Asd.java

---
+++ Asd.java
@@ -0,0 +1,13 @@
+class Asd
+{
+   public static void main(String args[])
+   {
+      System.out.println("Sum of the clones = " + z);
+      System.out.println("Sum of the clones = " + z);
+      System.out.println("Sum of the clones = " + z);
+      System.out.println("Sum of the clones = " + z);
+      System.out.println("Sum of the clones = " + z);
+      System.out.println("Sum of the clones = " + z);
+   }
+   
+}
